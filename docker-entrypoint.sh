6912398c1dfcc3cc20b27cd421af84378d5a61a1
Gabriel Totusek <skinlayers@gmail.com>
2022-11-04 19:22:22 -0700

    Fix docker-entrypoint.sh to hand removal of structures dir Update docker-entrypoint.sh to clean up broken symlinks and empty directories

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -e
+set -v
 
 # Ensure symlinks to critical resources exist
-for dir in behavior_packs definitions resource_packs structures; do
-    if ! [ -a "/data/$dir" ]; then
-        mkdir "/data/$dir"
+for dir in /minecraft/*/; do
+    dir_base="$(basename $dir)"
+    if ! [ -e "/data/$dir_base" ]; then
+        mkdir "/data/$dir_base"
     fi
-    for subdir in /minecraft/$dir/*; do
-        symlink="/data/$dir/$(basename $subdir)"
-        if ! [ -a "$symlink" ]; then
+    for subdir in /minecraft/$dir_base/*/; do
+        symlink="/data/$dir_base/$(basename $subdir)"
+        if ! [ -e "$symlink" ]; then
             ln -s "$subdir" "$symlink"
         fi
     done
 done
+
+# Clean up old/broken symlinks and empty directories
+#for subdir in /data/*/*; do
+#    if [ -L "$subdir" ]; then
+#        if ! [ -e "$subdir" ]; then
+#            rm "$subdir"
+#        fi
+#    fi
+#done
+find /data/ -xtype l -delete
+find /data/ -empty -type d -delete
 
 
 exec "$@"
